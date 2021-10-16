fe97863ee6476379a76099e607eb4486acc1a396
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-17 01:50:41 +0300

    add
    -task-8
    -task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyElem = document.querySelector("body");
+
+changeColorEl.addEventListener('click', changeColor)
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyElem.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
